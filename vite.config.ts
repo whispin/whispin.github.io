0cac7e5a2f9da062f933b917c2497e10bcec38e2
whispin <ray.zxh@gmail.com>
2025-07-09 02:31:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 export default defineConfig({
-    plugins: [vue()],
+  base: './', 
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html')
+    }
+  },
+  server: {
+    historyApiFallback: true // 注意：这是 express 中的用法，vite 内部并不使用 express
+  }
 });
