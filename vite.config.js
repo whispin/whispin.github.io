55b9be73c6bba7b701e6578906517047b8ee4639
whispin <ray.zxh@gmail.com>
2025-07-09 02:46:17 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import singleFile from 'vite-plugin-singlefile';
+
 export default defineConfig({
-    base: './',
-    plugins: [vue()],
-    build: {
-        rollupOptions: {
-            input: resolve(__dirname, 'index.html')
-        }
+  base: './', // 根据部署路径设定
+  plugins: [
+    vue(),
+    singleFile(), // 添加这个插件
+  ],
+  build: {
+    outDir: 'dist',
+    assetsInlineLimit: Infinity, // 避免生成 asset 文件
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+        manualChunks: () => 'everything.js', // 避免分包
+      }
     }
+  }
 });
