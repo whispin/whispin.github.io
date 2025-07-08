ab5ba05edbc475e95162307a5d4a84797573a7c5
whispin <ray.zxh@gmail.com>
2025-07-08 18:42:27 +0000

    update

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+export default defineConfig({
+    base: './',
+    plugins: [vue()],
+    build: {
+        rollupOptions: {
+            input: resolve(__dirname, 'index.html')
+        }
+    }
+});
